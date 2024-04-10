d087d04496c21807bc2365f6abdd7be73ffab68d
Theresa Walsh <Theresa.Walsh@bentley.com>
2024-04-10 15:51:38 -0400

    Add project files.

---
+++ UnitTestNullReference/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace UnitTestNullReference
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ UnitTestNullReference/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace UnitTestNullReference
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
